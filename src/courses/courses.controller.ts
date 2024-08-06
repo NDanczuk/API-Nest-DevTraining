0542e3023fde712b72b0f9f746ee3302587c4798
NDanczuk <nicolasdanc@gmail.com>
2024-08-06 15:21:53 -0300

    Configured Nest

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Course list' });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Course with id ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+}
