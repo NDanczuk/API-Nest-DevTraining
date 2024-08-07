d1a8283f6fb6424aee8aba063ca0fafe662eb3b4
NDanczuk <nicolasdanc@gmail.com>
2024-08-07 10:26:19 -0300

    DataBase connection configured

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 
-@Module({})
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'devtraining',
+  entities: [],
+  synchronize: true,
+};
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          ...dataSourceOptions,
+        };
+      },
+    }),
+  ],
+})
 export class DatabaseModule {}
