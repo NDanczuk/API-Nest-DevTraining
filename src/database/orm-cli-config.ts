260707654f2d8bcbb1c4d6dacbdb9aeab46216da
NDanczuk <nicolasdanc@gmail.com>
2024-08-07 15:15:54 -0300

    Tags migration added

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1723052246462 } from 'src/migrations/1723052246462-CreateCoursesTable';
+import { CreateTagsTable1723054244829 } from 'src/migrations/1723054244829-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1723052246462],
+  migrations: [CreateCoursesTable1723052246462, CreateTagsTable1723054244829],
 });
